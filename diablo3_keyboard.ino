53b2e5d34cef71c37bf57d77adf8502dc1cb011b
johnr <rayben@gmail.com>
2021-12-13 19:41:23 +0100

    Update diablo3_keyboard.ino
    Smaller delay, while loops removed.

--- diablo3_keyboard.ino
+++ diablo3_keyboard.ino
@@ -3,7 +3,7 @@
 
 void setup() {
   // make pin 2 an input and turn on the
-  // pullup resistor so it goes high unless
+  // pullup resistor so it goes LOW unless
   // connected to ground:
   pinMode(2, INPUT_PULLUP); // button 1
   pinMode(3, INPUT_PULLUP); // button 2
@@ -16,14 +16,10 @@ void setup() {
 }
 
 void loop() {
-  if (digitalRead(2) == LOW) { delay(65); Keyboard.print("1"); delay(65); }
-  if (digitalRead(3) == LOW) { delay(65); Keyboard.print("2"); delay(65); }
-  if (digitalRead(5) == LOW) { delay(65); Keyboard.print("3"); delay(65); }
-  if (digitalRead(6) == LOW) { delay(65); Keyboard.print("4"); delay(65); }
-  if (digitalRead(4) == LOW) {    
-    while( (digitalRead(2) == HIGH) || (digitalRead(3) == HIGH) || (digitalRead(5) == HIGH) || (digitalRead(6) == HIGH) )
-    { Mouse.click(MOUSE_LEFT);}  }
-  if (digitalRead(7) == LOW) { 
-    while( (digitalRead(2) == HIGH) || (digitalRead(3) == HIGH) || (digitalRead(5) == HIGH) || (digitalRead(6) == HIGH)) 
-    { Mouse.click(MOUSE_RIGHT);}  }
+  if (digitalRead(2) == LOW) { delay(33); Keyboard.print("1"); delay(33); }
+  if (digitalRead(3) == LOW) { delay(33); Keyboard.print("2"); delay(33); }
+  if (digitalRead(5) == LOW) { delay(33); Keyboard.print("3"); delay(33); }
+  if (digitalRead(6) == LOW) { delay(33); Keyboard.print("4"); delay(33); }
+  if (digitalRead(4) == LOW) { Mouse.click(MOUSE_LEFT); }
+  if (digitalRead(7) == LOW) { Mouse.click(MOUSE_RIGHT); }
 }
